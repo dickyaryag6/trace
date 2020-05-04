f027d5c7b2840dc7e137ca3bd574efac9a1c88b9
Dicky Arya Kesuma <dickyaryakesuma@gmail.com>
2020-05-04 18:24:56 +0700

    first commit

---
+++ tracer.go
@@ -0,0 +1,29 @@
+package trace
+
+import (
+	"io"
+	"fmt"
+)
+// Tracer is the interface that describes an object capable of
+// tracing events throughout code.
+type Tracer interface {
+	Trace(...interface{})
+}
+
+type tracer struct {
+	out io.Writer
+}
+func (t *tracer) Trace(a ...interface{}) {
+	// fmt.Print(a...)
+	
+	//write apa yang ada di a... ke t.out
+	fmt.Fprint(t.out, a...)
+
+	// fmt.Println(t.out)
+	fmt.Fprintln(t.out)
+}
+
+func New(w io.Writer) Tracer {
+	//create object tracer dengan atribut out
+	return &tracer{out: w}
+}
---
+++ tracer_test.go
@@ -0,0 +1,24 @@
+package trace
+
+import (
+	"bytes"
+	"testing"
+	// "fmt"
+)
+
+func TestNew(t *testing.T) {
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("should not be nil")
+	} else {
+		tracer.Trace("Hello trace package")
+		// buf.String() mengubah buffer menjadi string
+		// isinya sama dengan out
+		if buf.String() != "Hello trace package\n" {
+			t.Errorf("should not write '%s.", buf.String())
+		}
+		
+	}
+	
+}
